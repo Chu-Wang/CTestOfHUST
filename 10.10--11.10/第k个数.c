53e01e5f4db8be9d1a78cb8bc4c3fa84b5d09527
Chu-Wang <48759134+Chu-Wang@users.noreply.github.com>
2019-11-13 20:45:30 +0800

    Create 第k个数.c

---
+++ 10.10--11.10/第k个数.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include <stdlib.h>
+int NOK(unsigned n, int k)
+{
+	int a = 1, b = 1;
+	while (a <= k)
+	{
+		if ((n == 0) && (a <= k))
+		{
+			b = -1;
+			break;
+		}
+		b = n % 10;
+		n = n / 10;
+		a++;
+	}
+	return b;
+}
+int main(void)
+{
+	int y, m, p;
+	unsigned x;
+	m = scanf("%d%d", &x, &y);
+	while (m != EOF)
+	{
+		p = NOK(x, y);
+		printf("%d\n", p);
+		m = scanf("%d%d", &x, &y);
+	}
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
